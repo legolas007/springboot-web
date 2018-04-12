9b17e6f2553a4a2b7456a218b8d174b8db28b054
legolas007 <oukuanou@163.com>
2018-04-12 15:09:32 +0800

    嵌入servlet

---
+++ src/main/java/com/usher/springbootweb/config/MyServerConfig.java
@@ -0,0 +1,51 @@
+package com.usher.springbootweb.config;
+
+import com.usher.springbootweb.filter.MyFilter;
+import com.usher.springbootweb.listener.MyListener;
+import com.usher.springbootweb.servlet.MyServlet;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.ServletRegistration;
+import java.util.Arrays;
+
+@Configuration
+public class MyServerConfig {
+    //注册三大组件
+    @Bean
+    public ServletRegistrationBean myServlet(){
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(new MyServlet(),"/myServlet");
+        registrationBean.setLoadOnStartup(1);
+        return registrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean myFilter(){
+        FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+        registrationBean.setFilter(new MyFilter());
+        registrationBean.setUrlPatterns(Arrays.asList("/hello","/myServlet"));
+        return registrationBean;
+    }
+
+    @Bean
+    public ServletListenerRegistrationBean myListener(){
+        ServletListenerRegistrationBean<MyListener> registrationBean = new ServletListenerRegistrationBean<>(new MyListener());
+        return registrationBean;
+    }
+    //配置嵌入式的Servlet容器
+    @Bean
+    public EmbeddedServletContainerCustomizer embeddedServletContainerCustomizer(){
+        return new EmbeddedServletContainerCustomizer() {
+            @Override
+            public void customize(ConfigurableEmbeddedServletContainer configurableEmbeddedServletContainer) {
+                //定制嵌入式的Servlet容器相关的规则
+                configurableEmbeddedServletContainer.setPort(8083);
+            }
+        };
+    }
+}
